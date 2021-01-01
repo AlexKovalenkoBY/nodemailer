4f0d084ff338bcbdcc58a9e0173543d4154b89bf
Alexandr Kovalenko <kovalenkoam@gmail.com>
2021-01-02 00:22:49 +0300

    feat: almost production

--- From_Aris_with_Love_sendmail_.js
+++ From_Aris_with_Love_sendmail_.js
@@ -1,26 +1,29 @@
 "use strict";
 const nodemailer = require("nodemailer");
-import {a, b} from './my-module.js';
-a = 5;
-b = 6;
-//import {recipients} from './emails.js';
-//var nodemailer = require("nodemailer")
-//import  "./emails.js";
-//const recipients = require("./emails")
+const { recipients } = require('./emails');
+var fs = require('fs');
+//let file = null;
+let emailBody = null;
 
-/*const recipients = [
-  ["8_bezverhova_104" , "M.Bezverkhova@belapb.by" ],
-  ["8_chehovich_903" , "Чехович Юлия Петровна <Y.Chekhovich@belapb.by>"],
-  ["8_kovalenko_604", "a.kovalenko@belapb.by"] ,
-  ["8_Lyakina_468", "Лякина Н.А. [Проектно-процессный офис] <n.lyakina@belapb.by>"],
-  ["8_Packevich_032", "Пацкевич Д.А. [Управление инкассации] <d.packevich@belapb.by>"]
-  ];*/
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
+for (let i = 0; i < recipients.length; i++) {
+  
+   const filename = "./"+recipients[i][0] + '.txt';
+    //emailBody = null;
+    try {
+       emailBody = fs.readFileSync(filename, 'utf8');
+      console.log(emailBody);  
+      main(emailBody).catch(console.error)    ;  
+  } catch(e) {
+      console.log('Error:', e.stack);
+  }
+
+}
+
+//main("test");
+async function main(emailBody) {
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
- // let testAccount = await nodemailer.createTestAccount();
-
+  // let testAccount = await nodemailer.createTestAccount();
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "8-mail.belapb.by",
@@ -32,30 +35,29 @@ async function main() {
     },
 
   });
-// verify connection configuration
-transporter.verify(function(error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
+  // verify connection configuration
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: 'a.kovalenko@belapb.by', // sender address
     to: 'a.kovalenko@belapb.by', // list of receivers
-    subject: "Happy New Year!", // Subject line
-    text: "Hello! ✔ \n  From Aris with Love.... ", // plain text body
-    html: "<b>Hello! ✔ \n  From Aris with Love.... </b>", // html body
+    subject: "✔ From Aris with Love.... ", // Subject line
+    //text: "Hello! ✔ \n  From Aris with Love.... " , // plain text body
+    //text: ""+emailBody, // plain text body
+    html: "<br>✔ " +"<br>"+emailBody.split('\n').join('</br>')+ "</br>", // html body
+    //html: "<b>"+ emailBody + "</b>", // html body
 
   });
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
   // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
-
-main().catch(console.error);
